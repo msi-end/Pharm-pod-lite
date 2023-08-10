77317fe30519ce63fb2d684d71818b50ab71c235
InfoOrgs <aditya01377@gmail.com>
2023-08-10 18:46:45 +0530

    first

---
+++ app.js
@@ -0,0 +1,36 @@
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
+const StaticGzip = require('express-static-gzip');
+
+
+const PORT = process.env.PORT || "3001";
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.use('static', express.static(path.join(__dirname, "static")));
+
+app.use(compression({
+    level: 7,
+    threshold: 0
+}));
+app.use(
+    '/static',
+    StaticGzip(path.join(__dirname, '/static'), {
+        enableBrotli: true,
+        orderPreference: ['br', 'gz'],
+        setHeaders: (res, path) => {
+            res.setHeader('Cache-Control', 'public, max-age=31536000');
+        },
+    })
+)
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '/views/index.html'))
+});
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views/error.html'))
+})
+app.listen(PORT, () => { console.log('working on port 3001'); })
---
+++ static/js/main.js
@@ -0,0 +1,70 @@
+//SLIDER
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("carousel");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+// CALL NOW
+let callNow = document.querySelector(`#call-now`);
+let enquiry = document.querySelector(`#call-now span`);
+
+callNow.addEventListener("click", () => {
+  enquiry.classList.toggle(`active`);
+  callNow.classList.toggle(`active`);
+});
+// ABout Know More Text 
+const detailContainer = document.querySelector('.details');
+detailContainer.addEventListener('click', event=>{
+  const detail = event.target;
+  const isDetailsmoreBtn = detail.className.includes('detailsMore-btn');
+  if(!isDetailsmoreBtn) return;
+  const detailText = event.target.parentNode.querySelector('.detailsText');
+  detailText.classList.toggle('detailsText--show');
+  detail.textContent = detail.textContent.includes('Know More') ?
+  "Know Less...." : "Know More...."  
+})
+
+//APPOINTMENT FORM
+function openForm(){
+  let Form = document.querySelector('.form-section');
+  Form.style.display = 'block';
+}
+function closeForm() {
+  let form = document.querySelector(`.form-section`);
+  form.style.display = "none";
+}
+
+// Date Selection 
+function dSplit(val, p, t) { let [d, m, y] = val.split(p); return t ? `${y}/${m}/${d}` : `${y}-${m}-${d}` }
+function dateFn(e) {
+    let dtEm = document.getElementById(e); let tdy = new Date(); if (dtEm) {
+        let tdyVal = tdy.toLocaleDateString({ month: "2-digit", year: "numeric", day: "2-digit" })
+        let odt = new Date(tdy.setDate(tdy.getDate() - parseInt(dtEm.dataset.min))).toLocaleDateString({ month: "2-digit", year: "numeric", day: "2-digit" });
+        let ndt = new Date(tdy.setDate(tdy.getDate() + parseInt(dtEm.dataset.max) + parseInt(dtEm.dataset.min))).toLocaleDateString({ month: "2-digit", year: "numeric", day: "2-digit"});
+        console.log(dSplit(tdyVal, '/', false));
+        dtEm.defaultValue = dSplit(tdyVal, '/', false); dtEm.min = dSplit(odt, '/', false); dtEm.max = dSplit(ndt, '/', false);
+    }
+}dateFn('formDate');
+
+// ReviewOpen
+function openReview(){
+  document.querySelector(`.review-fill`).style.display= `block`;
+}
+
+//Review form 2nd part
+function openRevieweEnd(){
+  document.querySelector(`.review-form-end-section`).style.display = `block`;
+}
