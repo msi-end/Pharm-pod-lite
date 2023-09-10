896f17d91114e16e8ed27bd214369fd148c8b702
InfoOrgs <aditya01377@gmail.com>
2023-09-10 19:56:40 +0530

    final updates

--- static/js/script.js
+++ static/js/script.js
@@ -9,7 +9,6 @@ let ReqHandler = {
   },
   POST: async function (url, data) {
     // console.log(url)
-    console.log(url)
     const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json; charset=UTF-8", },
@@ -20,9 +19,6 @@ let ReqHandler = {
 let ReqURI = { FormSet: APImain + '/apiV2/crt?user=', GetDoc: `${APImain}/apiV2/getDoc?user=`, GetRevColl: APImain + '/apiV2/fn/', GetRevrat: APImain + '/apiV2/revv/', POSTRev: APImain + '/apiV2/rv'}
 async function displayDataWithRateandRev() {
   let dataWithRevRat = await ReqHandler.GET(ReqURI.GetRevColl + userName )
-let ReqURI = { FormSet: APImain + '/apiV2/crt?user=', GetDoc: `${APImain}/apiV2/getDoc?user=`, GetRevColl: APImain + '/apiV2/fn/', GetRevrat: APImain + '/apiV2/revv/', POSTRev: APImain + '/apiV2/rv' }
-async function displayDataWithRateandRev() {
-  let dataWithRevRat = await ReqHandler.GET(ReqURI.GetRevColl + userName)
   dataWithRevRat.data.forEach(ell => {
     let clinic = document.getElementById('heading').childNodes;
     clinic[1].innerHTML = `${ell.star.toFixed(1)}&#9733;`;
@@ -32,21 +28,14 @@ async function displayDataWithRateandRev() {
 async function DataOfRevrat() {
   let revRat = await ReqHandler.GET(ReqURI.GetRevrat + userName);
   let s = 1;
-  let s = 4;
   let box = document.getElementById('review')
-  console.log(revRat.data)
   revRat.data.forEach((el) => {
-    let str = '<label for="4" class="uis uis-star"></label> '
     let str4 = '<label for="4" class="uis uis-star"></label> <label for="4" class="uis uis-star"></label> <label for="4" class="uis uis-star"></label>  <label for="4" class="uis uis-star"></label>'
     let html = `<p><i class="uil uil-user-square"></i>
       <span>${el.userName}</span>
     </p>
     <div class="flexBox">
-    ${s === 1 ? str : ''}
-    ${s === 2 ? `${str}${str}` : ''}
-    ${s === 3 ? `${str}${str}${str}` : ''}
-    ${s === 4 ? `${str}${str}${str}${str}` : ''}
-    ${s === 5 ? `${str}${str}${str}${str}${str}` : ''}
+    ${str4}
     </div>
     <p id="review-section-text">${el.review}</p>
     <hr>`
@@ -136,24 +125,11 @@ function dSplit(val, p, t) { let [d, m, y] = val.split(p); return t ? `${y}/${m}
 
 const userReq = {
   FormSet: function (data) {
-    document.getElementById('circleLoad').style.display = `block`
-    document.getElementById('text').innerHTML = '';
-    ReqHandler.POST(ReqURI.FormSet + 'BreatheWellness', data).then((dat) => {
-      if (dat.status) {
-         document.getElementById('circleLoad').style.display = `none`
-         document.getElementById('text').innerHTML = 'Book Appoinment';
-         correctAlert(); myappointment(data) } else { wrongAlert(); }
-    document.querySelector(`#circleLoad`).style.display = `block`;
-    document.querySelector(`#text`).innerHTML = '';
-    ReqHandler.POST(ReqURI.FormSet + 'BreatheWellness', data).then((dat) => {
-      if (dat.status) { 
-        document.querySelector(`#circleLoad`).style.display = `none`
-        document.querySelector(`#text`).innerHTML = 'Book Appointment';
-        correctAlert(); myappointment(data) } else { wrongAlert(); }
+    ReqHandler.POST(ReqURI.FormSet + 'BreatheWellness', data).then((data) => {
+      if (data.status) { correctAlert(); } else { wrongAlert(); }
     })
   },
   CheckDoc: function () {
-    document.querySelector(`#loader`).style.display = `block`;
     let input_date = dSplit(document.getElementById('formDate').value, '-', true);
     ReqHandler.GET(ReqURI.GetDoc + 'BreatheWellness').then((e) => {
       let docCtn = document.getElementById('regnDoctor')
@@ -167,12 +143,9 @@ const userReq = {
             docCtn.innerHTML += `<option value="${e.data[i].d_name}">Dr. ${e.data[i].d_name}</option>`
           }
         }
-        document.querySelector(`#loader`).style.display = `none`;
-        
+
       } else { console.log('Get doc ,() not working') }
     })
-    
-
   }
 }
 
@@ -184,10 +157,10 @@ function proceed_bej() {
 }
 
 function fixApp() {
-  let dataform = document.querySelector('.form-second').childNodes[3];
-  console.log(dataform)
+  let dataform = document.querySelector('.form-second').childNodes[1];
   let ap_date = dSplit(doc[2].childNodes[3].value, '-', true)
-  let data = { name: doc[0].childNodes[3].value, number: doc[1].childNodes[3].value, doctor: dataform.childNodes[2].value, date: ap_date, otherInfo: doc[3].childNodes[3].value };
+  let data = { name: doc[0].childNodes[3].value, number: doc[1].childNodes[3].value, doctor: dataform.childNodes[5].value, date: ap_date, otherInfo: doc[3].childNodes[3].value };
+  // console.log(dataform.childNodes[5].value);
   userReq.FormSet(data);
 }
 function correctAlert() {
@@ -200,7 +173,6 @@ function wrongAlert() {
   document.querySelector(`#singu`).style.display = 'none';
   document.querySelector(`#leuy`).textContent = `Appointment is not Confirmed`;
   document.querySelector(`#aneo`).style.display = 'block';
-  document.querySelector(`#hula`).style.display = `none`;
   document.querySelector(`.form-section`).style.display = `none`;
 }
 
@@ -211,9 +183,6 @@ function d______T(param) {
   if (paramDate === new Date().toLocaleDateString()) {
     // console.log('Matching with today date');
     return true;} else {return false;}
-    console.log('Matching with today date');
-    return true;
-  } else { return false; }
 }
 
 function availDoc() {
@@ -232,7 +201,6 @@ function availDoc() {
       </p>
     </div>`;
     let htmlinactive = `<div class="avail-circle">
-      let htmlinactive = `<div class="avail-circle">
     <div class="image inactive">
       <img src="/static/img/doctors/${el.d_name.split(' ')[0].toLowerCase()}.jpg" alt="Doctor" class="" />
     </div>
@@ -245,56 +213,8 @@ function availDoc() {
      }else{container.innerHTML += htmlactive ;}
     });
   })
-      if (d) {
-        if (el.status == 'true') { container.innerHTML += htmlactive; } else { container.innerHTML += htmlinactive; }
-      } else { container.innerHTML += htmlactive; }
-    });
-  })
-}
-setTimeout(availDoc, 1000)
-
-function myappointment(data) {
-  if (localStorage.getItem('myappointment') === null) {
-    localStorage.setItem('myappointment', JSON.stringify({app1: data}));
-    let app = JSON.parse(localStorage.getItem('myappointment'));
-    renderAppo(app);
-  } else {
-    let app = JSON.parse(localStorage.getItem('myappointment'));
-    let newapp = `app${Object.keys(app).length + 1}`;
-    // let appo = JSON.parse(app, (key, value) => {return value;})
-    app[newapp] = data;
-    localStorage.setItem('myappointment', JSON.stringify(app));
-    renderAppo(app);
-  }
-}
-
-function renderAppo(info) {
-  for (const key in info) {
-    if (info.hasOwnProperty(key)) {
-      value = info[key];
-      console.log(value);
-      let html = `<div class="box">
-      <h3 class="name">${value.name}</h3>
-      <p class="number">${value.number}</p>
-      <p class="dr-name">${value.doctor}</p>
-      <p class="add-info"><span>Add. Info: </span>${value.otherInfo}</p>
-      <p class="app-status">Status: <span class="stat">Confirmed</span></p>
-      <p class="app-date">Date: <span>${value.date}</span></p>
-      <!-- ---- active class in stat makes it red-----  -->
-    </div>`
-    document.getElementById('my-appo').innerHTML += html; 
-    }
-  }
 }
 setTimeout(availDoc, 3000)
-
-
-
-
-
-
-
-
 
 
 
